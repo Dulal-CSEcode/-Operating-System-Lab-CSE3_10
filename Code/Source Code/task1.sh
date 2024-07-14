4934d946b3d2fa12a96d3805148b2f3da095f46e
MD Dulal Hossain <112533806+Dulal-CSEcode@users.noreply.github.com>
2024-07-14 19:27:39 +0600

    Add files via upload

---
+++ Code/Source Code/task1.sh
@@ -0,0 +1,9 @@
+echo "Enter the first string:"
+read string1
+
+echo "Enter the second string:"
+read string2
+
+concat_string="$string1$string2"
+
+echo "Concatenated string: $concat_string"
---
+++ Code/Source Code/work1.sh
@@ -0,0 +1,6 @@
+str1='dulal'
+str2='hossain'
+
+concat_string="$str1$str2"
+
+echo "concat string = :$concat_string"
