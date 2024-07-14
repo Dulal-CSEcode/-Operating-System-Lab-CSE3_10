ed3fca467f21cfd73a1d6977c92147a6a7dcfbfb
MD Dulal Hossain <112533806+Dulal-CSEcode@users.noreply.github.com>
2024-07-14 19:24:21 +0600

    Add files via upload

---
+++ Code/Source Code/Write a shell Script to Concatenate Two Strings.sh
@@ -0,0 +1,10 @@
+
+echo "Enter the first string:"
+read string1
+
+echo "Enter the second string:"
+read string2
+
+concatenated_string="$string1$string2"
+
+echo "Concatenated string: $concatenated_string"
