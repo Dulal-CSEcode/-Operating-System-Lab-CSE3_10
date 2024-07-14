cd1f2eaca01c769391db3237c19d1b54e96a1708
MD Dulal Hossain <112533806+Dulal-CSEcode@users.noreply.github.com>
2024-07-14 19:19:04 +0600

    Add files via upload

---
+++ Code/Commands/addition.sh
@@ -0,0 +1,9 @@
+echo "Enter the 1st number : "
+read a 
+
+echo "Enter the 2nd number : "
+read b
+
+sum=$(($a+$b))
+
+echo "sum of two Numbers is : $sum"
---
+++ Code/Commands/sub.sh
@@ -0,0 +1,9 @@
+echo "Enter the 1st number : "
+read a 
+
+echo "Enter the 2nd number : "
+read b
+
+sub=$(($a-$b))
+
+echo "sub of two Numbers is : $sub"
